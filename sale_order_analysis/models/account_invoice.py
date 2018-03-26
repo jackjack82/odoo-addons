89004effaadfdf54d1f680a1e7d3e9d419aa2f78
jackjack <giacomo.grasso.82@gmail.com>
2018-03-26 09:06:06 +0200

    debugged sale_order_analysis

--- sale_order_analysis/models/account_invoice.py
+++ sale_order_analysis/models/account_invoice.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-#   @author Giacom Grasso <giacomo.grasso.82@gmail.com>
+#   @author Giacomo Grasso <giacomo.grasso.82@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
@@ -11,41 +11,22 @@ class AccountInvoice(models.Model):
     """
     When an invoice is edited, the related sale orders indicators are updated
     """
-    """
-    test = fields.Boolean("test", compute='_test', store=True)
 
-    @api.multi
-    @api.depends('residual')
-    def _test(self):
-
-        sale_orders = self.mapped('invoice_line_ids.sale_line_ids.order_id')
-        for order in sale_orders:
-            order._get_order_paid_amount()
-
-    @api.multi
-    def write(self, vals):
-        res = super(AccountInvoice, self).write(vals)
+    @api.one
+    @api.depends(
+        'state', 'currency_id', 'invoice_line_ids.price_subtotal',
+        'move_id.line_ids.amount_residual',
+        'move_id.line_ids.currency_id')
+    def _compute_residual(self):
+        res = super(AccountInvoice, self)._compute_residual()
 
         sale_orders = self.mapped('invoice_line_ids.sale_line_ids.order_id')
         for order in sale_orders:
-            order._get_order_paid_amount()
+            amount_paid, amount_unpaid = order._get_order_paid_amount()
 
-        return res
-    """
-
-class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
-
-    """
-    When an invoice is edited, the related sale orders indicators are updated
-    """
-
-    @api.multi
-    def write(self, vals):
-        res = super(AccountMoveLine, self).write(vals)
-
-        sale_orders = self.mapped('invoice_id.invoice_line_ids.sale_line_ids.order_id')
-        for order in sale_orders:
-            order._get_order_paid_amount()
+            order.write({
+                'order_amount_paid': amount_paid,
+                'order_amount_to_pay': order.amount_total - amount_paid,
+            })
 
         return res
