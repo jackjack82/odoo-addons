ce53ca01732c69d2a6b5224513e3c64373b49283
jackjack <giacomo.grasso.82@gmail.com>
2018-10-18 16:23:05 +0200

    [GEN] cleaning repository

--- sale_order_analysis/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-#   @author Giacom Grassso <giacomo.grasso.82@gmail.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import account_invoice
-from . import sale_order
-from . import sale_order_line
--- sbl_sale_old/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding=utf-8
-
-from . import account
-from . import model
-from . import sale
