96b959fdd2a71dd769bfae6fa7f632a0ec463a18
jackjack82 <giacomo.grasso.82@gmail.com>
2019-07-02 09:50:27 +0200

    [10][italy_currency_update] module finalized

---
+++ italy_currency_update/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Giacomo Grasso, Gabriele Baldessari
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "ITA - Aggiornamento valute Banca d'Italia",
+    'version': '10.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'summary': 'Modulo per aggiornare i tassi di cambio automaticamente '
+               'con i dati della Banca d\'Italia.',
+    'author': 'Giacomo Grasso, Gabriele Baldessari,',
+    'website': 'https://www.odoominds.com/',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+        'currency_rate_update'],
+    'data': [],
+    'installable': True,
+    'auto-install': False,
+}
